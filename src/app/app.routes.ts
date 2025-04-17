0fc23ecbc43467f210147f37c8e81cbbcf13f643
Marcel Menke <marcel.menke1981@gmail.com>
2025-04-17 12:01:47 +0200

    Update app.routes.ts
    added routes ( Lazy Loading )

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    title: 'JobTracker - Dashboard'
+  },
+  {
+    path: 'applications/new', 
+    loadComponent: () => import('./components/application-form/application-form.component').then(m => m.ApplicationFormComponent),
+    title: 'JobTracker - Neue Bewerbung'
+  },
+  {
+    path: 'applications/:id/edit', 
+    loadComponent: () => import('./components/application-form/application-form.component').then(m => m.ApplicationFormComponent),
+    title: 'JobTracker - Bewerbung bearbeiten'
+  },
+  {
+    path: 'applications',
+    loadComponent: () => import('./features/applications/applications.component').then(m => m.ApplicationsComponent),
+    title: 'JobTracker - Bewerbungen'
+  },
+  {
+    path: 'documents',
+    loadComponent: () => import('./features/documents/documents.component').then(m => m.DocumentsComponent),
+    title: 'JobTracker - Dokumente'
+  },
+  {
+    path: 'calendar',
+    loadComponent: () => import('./features/calendar/calendar.component').then(m => m.CalendarComponent),
+    title: 'JobTracker - Kalender'
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
