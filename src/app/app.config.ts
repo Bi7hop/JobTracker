30ce903d1ebb387e7ebf7f2212636e5f475d75eb
Marcel Menke <marcel.menke1981@gmail.com>
2025-04-22 08:05:48 +0200

    Update app.config.ts
    - added calendar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core'; 
+import { provideRouter } from '@angular/router'; 
 import { provideAnimations } from '@angular/platform-browser/animations';
-
 import { routes } from './app.routes';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+registerLocaleData(localeDe); 
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimations() 
+    provideAnimations(),
+    importProvidersFrom(
+       CalendarModule.forRoot({
+         provide: DateAdapter,
+         useFactory: adapterFactory 
+       })
+    ),
+    { provide: LOCALE_ID, useValue: 'de-DE' }
   ]
 };
