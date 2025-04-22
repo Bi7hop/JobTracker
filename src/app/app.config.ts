8fa3d1f818c5b4f70c7161cfd02efa25bdf9a74b
Marcel Menke <marcel.menke1981@gmail.com>
2025-04-22 09:43:12 +0200

    Update app.config.ts
    - added toastr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,30 @@
-import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core'; 
-import { provideRouter } from '@angular/router'; 
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations'; 
 import { routes } from './app.routes';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { provideToastr } from 'ngx-toastr';
 import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
-registerLocaleData(localeDe); 
+registerLocaleData(localeDe);
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
+    provideToastr({
+      timeOut: 3000, 
+      positionClass: 'toast-bottom-center', 
+      preventDuplicates: true, 
+      progressBar: true, 
+    }),
     importProvidersFrom(
-       CalendarModule.forRoot({
-         provide: DateAdapter,
-         useFactory: adapterFactory 
-       })
+       CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
     ),
+    
     { provide: LOCALE_ID, useValue: 'de-DE' }
+  
   ]
 };
