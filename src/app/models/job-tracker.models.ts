4ee7eafe01a8a1d5c5a58a618da6b4b0f74b40de
Marcel Menke <marcel.menke1981@gmail.com>
2025-04-22 08:08:38 +0200

    Update job-tracker.models.ts

--- src/app/models/job-tracker.models.ts
+++ src/app/models/job-tracker.models.ts
@@ -1,26 +1,27 @@
 export interface Stat {
-    title: string;
-    value: number;
-    total?: number;
-    color: string;
-    borderColor: string;
-  }
-  
-  export interface Application {
-    id?: string | number; 
-    company: string;
-    location: string;
-    position: string;
-    status: string;
-    date: string;
-    color: string;
-  }
-  
-  export interface Event {
-    title: string;
-    company: string;
-    time: string;
-    isToday: boolean;
-    color: string;
-  }
-  
+  title: string;
+  value: number;
+  total?: number;
+  color: string;
+  borderColor: string;
+}
+
+export interface Application {
+  id?: string | number; 
+  company: string;
+  location: string;
+  position: string;
+  status: string;
+  date: string;
+  color: string;
+}
+
+export interface Event {
+  id: string | number; 
+  title: string;
+  company: string;
+  time: string;
+  date: string; 
+  color: string; 
+}
+
