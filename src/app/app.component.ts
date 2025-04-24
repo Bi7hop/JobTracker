16bcafaddf7f63e69c74462b5c7385c53536819c
Marcel Menke <marcel.menke1981@gmail.com>
2025-04-24 17:12:45 +0200

    added reminder to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { ReminderCheckService } from './services/reminder-check.service';
+import { ReminderNotificationModalComponent } from './shared/components/reminder-notification-modal/reminder-notification-modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    HeaderComponent,
+    ReminderNotificationModalComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'JobTracker';
+  
+  constructor(public reminderCheckService: ReminderCheckService) {}
+  
+  ngOnInit() {
+    this.reminderCheckService.startChecking();
+  }
+  
+  closeReminderNotification() {
+    this.reminderCheckService.dismissCurrentReminder();
+  }
+  
+  markReminderComplete(id: string) {
+    this.reminderCheckService.markReminderComplete(id);
+  }
+  
+  snoozeReminder(data: { id: string, minutes: number }) {
+    this.reminderCheckService.snoozeReminder(data.id, data.minutes);
+  }
 }
